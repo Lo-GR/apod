663cccc73a5a5943e726cfb81d1f02c9ddd2d148
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-10-20 16:32:51 -0700

    remove additional css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-  <div class="flex-shrink-0">
-  </div>
-  <div>
-    <div class="text-xl font-medium text-black">ChitChat</div>
-    <p class="text-gray-500">You have a new message!</p>
-  </div>
-</div>
+    <div>
+
     </div>
   );
 }
