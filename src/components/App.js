8b0029dbbe7cb547f226ec4014086aae77d46125
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-10-27 14:45:47 -0700

    add stored state for date

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import DateForm from "./DateForm";
+import React, { useState } from "react"
 
 function App() {
+  const [date, setDate] = useState("empty")
   return (
-    <div className="flex items-center justify-center h-screen w-screen">
-      <DateForm />
+    <div className="flex items-center justify-center h-screen w-screen flex-col">
+      <DateForm setDate={setDate}/>
+      <button onClick={() => console.log(date)} >
+        Test
+      </button>
     </div>
   );
 }
--- src/components/DateForm.js
+++ src/components/DateForm.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function DateForm() {
+export default function DateForm({ setDate }) {
   return (
     <div className="flex items-center">
-      <input type="date" />
+      <input type="date" onChange={(e) => setDate(e.target.value)}/>
     </div>
   )
 }
