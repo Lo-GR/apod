f07f4fe1bc3d50a186df52777d131a227d26665a
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-10-27 15:56:55 -0700

    connect app to nasa api

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,20 @@ import DateForm from "./DateForm";
 import React, { useState } from "react"
 
 function App() {
-  const [date, setDate] = useState("empty")
+  const [date, setDate] = useState("empty");
+  const [currentImage, setCurrentImage] = useState("");
+  const handleGetAPOD = async () => {
+    await fetch (`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_API_KEY}&count=1`, {
+      method: "GET",
+    })
+      .then(response => response.json())
+      .then(data => console.log(data[0].url))
+      .catch(err => console.log(err))
+  }
   return (
     <div className="flex items-center justify-center h-screen w-screen flex-col">
       <DateForm setDate={setDate}/>
-      <button onClick={() => console.log(date)} >
+      <button onClick={handleGetAPOD} >
         Test
       </button>
     </div>
