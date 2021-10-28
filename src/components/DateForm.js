af1773fd95b6c122500eb9e9d7622f6186593420
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-10-28 15:04:59 -0700

    styling to date picker

--- src/components/DateForm.js
+++ src/components/DateForm.js
@@ -3,7 +3,19 @@ import React from 'react'
 export default function DateForm({ setDate }) {
   return (
     <div className="flex items-center">
-      <input type="date" onChange={(e) => setDate(e.target.value)}/>
+      <input className="
+        text-3xl
+        w-full
+        pl-4
+        pr-10
+        py-3
+        rounded-xl
+        shadow-sm
+        focus:outline-none focus:shadow-outline
+        text-white
+        bg-gray-700
+        font-medium"
+        type="date" onChange={(e) => setDate(e.target.value)}/>
     </div>
   )
 }
