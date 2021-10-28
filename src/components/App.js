8da6ee56a3cdf1245ab5594cf0abad175c387b97
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-10-28 14:38:42 -0700

    connect date search logic with api

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,20 @@ import DateForm from "./DateForm";
 import React, { useState } from "react"
 
 function App() {
-  const [date, setDate] = useState("empty");
+  const [date, setDate] = useState(null);
   const [currentImage, setCurrentImage] = useState(null);
   const handleGetAPOD = async () => {
-    await fetch (`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_API_KEY}&count=1`, {
-      method: "GET",
-    })
-      .then(response => response.json())
-      .then(data => setCurrentImage(data[0].url))
-      .catch(err => console.log(err))
+    if(!date){
+      console.log("no date selected")
+    } else{
+      console.log(date)
+      await fetch (`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_API_KEY}&date=${date}`, {
+        method: "GET",
+      })
+        .then(response => response.json())
+        .then(data => setCurrentImage(data.url))
+        .catch(err => console.log(err))
+    }
   }
   return (
     <div className="flex items-center justify-center h-screen w-screen flex-col">
@@ -18,7 +23,7 @@ function App() {
       <button onClick={handleGetAPOD} >
         Test
       </button>
-      {!currentImage ? null : <img src={currentImage} alt="space picture" />}
+      {!currentImage ? null : <img src={currentImage} alt="NASA Space" />}
     </div>
   );
 }
