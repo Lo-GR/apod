d3de50ecd20a20e38ab8e8360365958a56414956
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-11-01 15:54:20 -0700

    add random capability

--- src/components/App.js
+++ src/components/App.js
@@ -15,9 +15,17 @@ function App() {
         method: "GET",
       })
         .then(response => response.json())
-        .then(data => setCurrentImage(data.url))
+        .then(data => setCurrentImage(data[0].url))
         .catch(err => console.log(err))
     }
+  }
+  const handleGetRandomAPOD = async () => {
+    await fetch (`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_API_KEY}&count=1`, {
+      method: "GET",
+    })
+      .then(response => response.json())
+      .then(data => setCurrentImage(data[0].url))
+      .catch(err => console.log(err))
   }
   return (
     <div className="flex items-center justify-center h-screen w-screen flex-col overflow-hidden">
@@ -25,7 +33,7 @@ function App() {
       <DateForm setDate={setDate}/>
       <div className="flex flex-row w-2/6 mt-8 justify-evenly items-center">
         <TWButton click={handleGetAPOD} text="Get APOD" />
-        <TWButton click={handleGetAPOD} text="Random APOD" />
+        <TWButton click={handleGetRandomAPOD} text="Random APOD" />
       </div>
       {!currentImage ? null : <img src={currentImage} className=" z-10" alt="NASA Space" />}
     </div>
