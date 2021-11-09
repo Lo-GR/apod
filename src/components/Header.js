6a8899f7e888a7ea95202c56dcdb900987e5fabc
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-11-09 15:54:33 -0800

    amend styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,19 @@ import DateForm from "./DateForm";
 export default function Header({ handleGetAPOD, handleGetRandomAPOD, currentImage, setDate}) {
   return (
     <div className="w-screen bg-white h-20 fixed top-0 justify-between flex flex-row items-center px-10 border-solid border-1 border-indigo-900 z-20">
-      <h1 className="font-serif font-black text-lg">Astronomy Picture of the Day</h1>
+      <h1 className="font-serif font-black text-lg font-bold">Astronomy Picture of the Day</h1>
       {!currentImage ? 
         null :
-        <div className="flex flex-row">
-          <DateForm setDate={setDate} />
-          <TWButton click={handleGetAPOD} text="Get APOD" />
-          <TWButton click={handleGetRandomAPOD} text="Random APOD" />
+        <div className="flex flex-row justify-around w-2/5">
+          <div className="mx-2">
+            <DateForm setDate={setDate} />
+          </div>
+          <div className="mx-2">
+            <TWButton click={handleGetAPOD} text="Get APOD" />
+          </div>
+          <div className="mx-2">
+            <TWButton click={handleGetRandomAPOD} text="Random APOD" />
+          </div>
         </div>
       }
       <p>test</p>
--- src/components/ImageInfo.js
+++ src/components/ImageInfo.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function ImageInfo({ explanation, date, title }) {
   return (
-    <div className="flex bg-white w-2/5 rounded-lg mx-5 p-5 justify-evenly flex-col items-baseline">
-      <h3>{title}</h3>
-      <p>{date}</p>
-      <p>{explanation}</p>
+    <div className="flex bg-white w-2/5 rounded-lg mx-5 p-10 justify-evenly flex-col items-baseline">
+      <h2 className="text-gray-700 text-6xl my-2">{title}</h2>
+      <p className="text-gray-700 text-lg my-2">{date}</p>
+      <p className="text-gray-700 my-2">{explanation}</p>
     </div>
   )
 }
